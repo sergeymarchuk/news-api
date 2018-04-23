a733059e6bf01ddef6156d0326d666b6ae097271
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-04-24 00:22:56 +0300

    add opportunity to find class without namespace

--- application/config/Autoloader.php
+++ application/config/Autoloader.php
@@ -3,19 +3,24 @@
 class Autoloader {
     private $baseDir;
     private $baseNamespace;
-
+    private $structure = array();
 
     public function __construct($baseDir, $baseNamespace = null) {
         $this->baseDir = trim($baseDir, " \t\n\r\0\x0B\\") . DS . $baseNamespace;
         $this->baseNamespace = $baseNamespace;
+        $this->init($baseDir . DS . 'application');
     }
 
     public function load($className) {
-        $class = $this->clearNamespace($className);
-        $file = $this->baseDir . DS . str_replace('\\', DS, $class) . '.php';
+        $class = str_replace('\\', DS, $this->clearNamespace($className)) . '.php';
+        $file = $this->baseDir . DS . $class;
 
         if (!file_exists($file)) {
-            die ("class \"{$className}\" is not found");
+            if ($this->findFile($class) === NULL) {
+                die ("class \"{$className}\" is not found");
+            }
+            
+            $file = $this->findFile($class);
         }
         require_once $file;
     }
@@ -27,5 +32,24 @@ class Autoloader {
         }
 
         return trim($class, " \t\n\r\0\x0B\\");
+    }
+
+    private function init($baseDir) {
+        $list = array_filter(scandir($baseDir), function ($value) {if (!in_array($value, array('.','..'))) {return true;} });
+
+        foreach ($list as $value) {
+            if (is_dir($baseDir . DS . $value)) {
+                array_push($this->structure, $baseDir . DS . $value);
+                $this->init($baseDir . DS . $value);
+            }
+        }
+    }
+
+    private function findFile($file) {
+        foreach ($this->structure as $path) {
+            if (file_exists($path . DS . $file)) {
+                return $path . DS . $file;
+            }
+        }
     }
 }

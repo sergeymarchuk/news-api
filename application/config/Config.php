7848f4d6e5ba14acfbb56db870f30cbce8d8584a
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-04-26 00:15:34 +0300

    append setting default (include set sources_list)

--- application/config/Config.php
+++ application/config/Config.php
@@ -11,4 +11,32 @@ class Config {
     public static function set($key, $value) {
         self::$settings[$key] = $value;
     }
+
+    public static function initDefaultSettings() {
+
+        if ( empty(self::$settings) ) {
+            $default_config = require_once 'app.conf.php';
+
+            foreach ($default_config as $key => $value) {
+                self::set($key, $value);
+            }
+
+            self::setSources();
+        }
+    }
+
+    private static function setSources() {
+        $sources_list = array();
+
+        $request_string = "https://newsapi.org/v2/sources?apiKey=" . self::$settings['api_key'];
+        $sources = json_decode(file_get_contents($request_string));
+
+        if ($sources->status == 'ok') {
+            foreach ($sources->sources as $value) {
+                $sources_list[$value->id] = $value->name;
+            }
+        }
+
+        self::set('sources_list', $sources_list);
+    }
 }

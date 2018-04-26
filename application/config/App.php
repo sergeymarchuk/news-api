662a6f661ae192671c70c6ce62a5e9e3238fa9a2
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-04-26 23:38:36 +0300

    change init method realise display content in App::run() method

--- application/config/App.php
+++ application/config/App.php
@@ -7,14 +7,27 @@ class App {
 
     public static function run() {
         self::init();
+
+        $controller_class = ucfirst(self::$route->getController()) . 'Controller';
+        $controller_action = self::$route->getAction() . 'Action';
+        $controllerObject = new $controller_class();
+
+        if (method_exists($controllerObject, $controller_action)) {
+            $view_path = $controllerObject->$controller_action();
+            $view_object = new BaseView($controllerObject->getData(), $view_path);
+            $content = $view_object->render();
+        }
+
+        $layout_path = self::$route->getLayout();
+        $layout_view_object = new BaseView(compact('content'), $layout_path);
+
+        echo $layout_view_object->render();
     }
 
     public static function init() {
-        $conf = require_once 'conf.application.php';
-        self::$route = new Router($_SERVER['REQUEST_URI'], $conf['routes']);
+        Config::initDefaultSettings();
 
-        Config::set('name_site', $conf['name_site']);
-        Config::set('language', $conf['language']);
+        self::$route = new Router($_SERVER['REQUEST_URI']);
     }
 
     public static function getRoute() {

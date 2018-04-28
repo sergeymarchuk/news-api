308955874b1497da958f939e0a566b97b42389ac
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-04-28 10:05:37 +0300

    defined VIEW_PATH constant

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
     define ('DS', DIRECTORY_SEPARATOR);
     define ('ROOT', dirname(dirname(__FILE__)));
+    define ('VIEW_PATH', ROOT . DS . 'application' . DS . 'views');
 
     require_once ROOT . DS . 'application' . DS . 'config' . DS . 'Autoloader.php';
     $loader = new Autoloader(ROOT, 'application');

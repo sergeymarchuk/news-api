eb1723f3e82a2a47d13dc91c51cdbb093da314c1
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-05-04 01:00:37 +0300

    create form for saving preferences

--- application/views/user/preferences.php
+++ application/views/user/preferences.php
@@ -1,5 +1,33 @@
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-5">My preferences</h1>
+        <h4>Name 1</h4>
+        <div>Country: Country1, Country2</div>
+        <div>Sources: Source1, Source2</div>
+    </div>
+</div>
+
 <form action="">
-    <?php foreach (application\config\Config::get('country_list') as $key => $value) {?>
-    <input type="radio" name="country" value="<?= $value ?>"> <?= $key ?> <br>
-    <?php }?>
+    <div class="form-group">
+        <label>Enter name</label>
+        <input type="text" class="form-control" placeholder="Preference name">
+    </div>
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label>Choose country</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <?php foreach (application\config\Config::get('country_list') as $key => $value) {?>
+                    <option name="country" value="<?= $value ?>"><?= $key ?></option>
+                <?php }?>
+            </select>
+        </div>
+        <div class="form-group col-md-6">
+            <label >Choose sources</label>
+            <select multiple class="form-control">
+                <<?php foreach (application\config\Config::get('sources_list') as $key => $value) {?>
+                    <option name="country" value="<?= $value ?>"><?= $key ?></option>
+                <?php }?>
+            </select>
+        </div>
+    </div>
 </form>

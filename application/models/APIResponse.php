cd7d90bfff31304838db1c174c1f4fa2873878f1
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-05-04 00:58:08 +0300

    add arg number_page for method getTopHeadlines()

--- application/models/APIResponse.php
+++ application/models/APIResponse.php
@@ -5,10 +5,10 @@ namespace application\models;
 use application\config\Config;
 
 class APIResponse {
-    private $top_headlines_string ='https://newsapi.org/v2/top-headlines?country=%2$s&apiKey=%1$s';
+    private $top_headlines_string ='https://newsapi.org/v2/top-headlines?country=%2$s&page=%3$s&apiKey=%1$s';
 
-    public function getTopHeadlines() {
-        $request_string =  sprintf($this->top_headlines_string, Config::get('api_key'), Config::get('country'));
+    public function getTopHeadlines($number_page = 1) {
+        $request_string =  sprintf($this->top_headlines_string, Config::get('api_key'), Config::get('country'), $number_page);
         $news_list = json_decode(file_get_contents($request_string));
 
         if ($news_list->status == 'ok') {
